74b61af32159e1d69603e5cb25e8c4550e05d03d
Rudger Gravestein <r.gravestein@triple-networks.com>
2021-04-15 16:30:26 +0200

    Update latest version

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 'use strict';
 
 exports.handler = (event, context, callback) => {
+    // only media starts with /media, all other are /static
 
-    // Get contents of response
+
+
+    // Get contents of request + response
+    const request = event.Records[0].cf.request;
     const response = event.Records[0].cf.response;
     const headers = response.headers;
+
+    let cacheControleDirective;
+    let headerValueMaxAge;
 
     // Set new security headers
     headers['strict-transport-security'] = [{ key: 'Strict-Transport-Security', value: 'max-age= 63072000; includeSubdomains; preload' }];
     headers['x-content-type-options'] = [{ key: 'X-Content-Type-Options', value: 'nosniff' }];
     headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: 'DENY' }];
     headers['x-xss-protection'] = [{ key: 'X-XSS-Protection', value: '1; mode=block' }];
-
-    // headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'" }];
-    // headers['referrer-policy'] = [{ key: 'Referrer-Policy', value: 'same-origin' }];
 
     // Cache-Control directives
     const cacheControleDirectiveNoCache = 'no-cache';
@@ -23,62 +27,86 @@ exports.handler = (event, context, callback) => {
 
     // MaxAge default times
     const headerNameMaxAge = 'max-age';
+    const headerValueMaxAgeZero = '0';
     const headerValueMaxAgeHour = '3600';
     const headerValueMaxAgeDay = '86400';
     const headerValueMaxAgeMonth = '2592000';
     const headerValueMaxAgeYear = '31536000';
 
-    // Set the max-age header, if not set.
-    const headerValueMaxAge = headerValueMaxAgeDay;
+    if (typeof request === 'undefined') {
+        cacheControleDirective = cacheControleDirectiveNoStore;
+        headerValueMaxAge = headerValueMaxAgeZero;
+        // "no-store, max-age=0"
+    } else if (request.uri.startsWith('/media/mgs/css')) {
+        cacheControleDirective = cacheControleDirectiveNoCache;
+        headerValueMaxAge = headerValueMaxAgeHour;
+        // "no-cache, max-age=3600"
+    } else if (request.uri.startsWith('/media')) {
+        cacheControleDirective = cacheControleDirectivePublic;
+        headerValueMaxAge = headerValueMaxAgeMonth;
+        // "public, max-age=2592000"
+    }  else if (request.uri.startsWith('/_cache/merged')) {
+        cacheControleDirective = cacheControleDirectivePublic;
+        headerValueMaxAge = headerValueMaxAgeYear;
+        // "public, max-age=31536000"
+    } else if (request.uri.startsWith('/frontend/Mgs/ninth/nl_NL/fonts')) {
+        cacheControleDirective = cacheControleDirectivePublic;
+        headerValueMaxAge = headerValueMaxAgeYear;
+        // "public, max-age=31536000"
+    } else if (request.uri.startsWith('/frontend')) {
+        cacheControleDirective = cacheControleDirectivePublic;
+        headerValueMaxAge = headerValueMaxAgeDay;
+        // "public, max-age=86400"
+    } else {
+        cacheControleDirective = cacheControleDirectiveNoCache;
+        headerValueMaxAge = headerValueMaxAgeDay;
+        // "no-cache, max-age=86400"
+    }
 
-    // if (!headers[headerNameMaxAge.toLowerCase()]) {
-    //     headers[headerNameMaxAge.toLowerCase()] = [{
-    //         key: headerNameMaxAge,
-    //         value: headerValueMaxAge,
+    // Set the Cache-Control header, if not set.
+    const headerNameCacheControl = 'Cache-Control';
+    const headerValueCacheControl = cacheControleDirective + ', '
+        + 'max-age=' + headerValueMaxAge;
+
+    // console.log('headerValueCacheControl', headerValueCacheControl);
+    headers[headerNameCacheControl.toLowerCase()] = [{
+        key: headerNameCacheControl,
+        value: headerValueCacheControl,
+    }];
+
+    // if (!headers[headerNameCacheControl.toLowerCase()]) {
+    //     headers[headerNameCacheControl.toLowerCase()] = [{
+    //         key: headerNameCacheControl,
+    //         value: headerValueCacheControl,
     //     }];
     // }
 
-  // Set the Cache-Control header, if not set.
-  // "max-age=86400, public"
-  const headerNameCacheControl = 'Cache-Control';
-  const headerValueCacheControl = cacheControleDirectivePublic + ', '
-      + 'max-age=' + headerValueMaxAge;
-
-  if (!headers[headerNameCacheControl.toLowerCase()]) {
-        headers[headerNameCacheControl.toLowerCase()] = [{
-            key: headerNameCacheControl,
-            value: headerValueCacheControl,
-        }];
-    }
-
-    const headerNameAccessControlAllowHeaders = 'Access-Control-Allow-Headers';
-    const headerValueAccessControlAllowHeaders = '*';
-
-    if (!headers[headerNameAccessControlAllowHeaders.toLowerCase()]) {
-        headers[headerNameAccessControlAllowHeaders.toLowerCase()] = [{
-            key: headerNameAccessControlAllowHeaders,
-            value: headerValueAccessControlAllowHeaders,
-        }];
-    }
-
-    const headerNameAccessControlAllowMethods = 'Access-Control-Allow-Methods';
-    const headerValueAccessControlAllowMethods = 'POST, GET, OPTIONS';
-    if (!headers[headerNameAccessControlAllowMethods.toLowerCase()]) {
-        headers[headerNameAccessControlAllowMethods.toLowerCase()] = [{
-            key: headerNameAccessControlAllowMethods,
-            value: headerValueAccessControlAllowMethods,
-        }];
-    }
-
     const headerNameAccessControlAllowOrigin = 'Access-Control-Allow-Origin';
     const headerValueAccessControlAllowOrigin = '*';
-
     if (!headers[headerNameAccessControlAllowOrigin.toLowerCase()]) {
         headers[headerNameAccessControlAllowOrigin.toLowerCase()] = [{
             key: headerNameAccessControlAllowOrigin,
             value: headerValueAccessControlAllowOrigin,
         }];
     }
+
+    // const headerNameAccessControlAllowHeaders = 'Access-Control-Allow-Headers';
+    // const headerValueAccessControlAllowHeaders = '*';
+    // if (!headers[headerNameAccessControlAllowHeaders.toLowerCase()]) {
+    //     headers[headerNameAccessControlAllowHeaders.toLowerCase()] = [{
+    //         key: headerNameAccessControlAllowHeaders,
+    //         value: headerValueAccessControlAllowHeaders,
+    //     }];
+    // }
+
+    // const headerNameAccessControlAllowMethods = 'Access-Control-Allow-Methods';
+    // const headerValueAccessControlAllowMethods = 'POST, GET, OPTIONS';
+    // if (!headers[headerNameAccessControlAllowMethods.toLowerCase()]) {
+    //     headers[headerNameAccessControlAllowMethods.toLowerCase()] = [{
+    //         key: headerNameAccessControlAllowMethods,
+    //         value: headerValueAccessControlAllowMethods,
+    //     }];
+    // }
 
     // Return modified response
     callback(null, response);
